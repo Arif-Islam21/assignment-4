0e7fdb7379f9720a5f2d60d8349e4b8d6f0f9964
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-07 21:37:52 +0600

    task 1 done for assignment

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function calculateMoney(ticketSale) {
-  const ticketPrice = 120;
-  const lunchMoney = 50;
-  const stuff = 8;
-  const gateman = 500;
-  const expenses = gateman + stuff * lunchMoney;
-  const earning = ticketSale * ticketPrice;
-  const saving = earning - expenses;
-  return saving;
+  if (ticketSale < 0) {
+    const invalid = "Invalid Number";
+    return invalid;
+  } else {
+    const ticketPrice = 120;
+    const lunchMoney = 50;
+    const stuff = 8;
+    const gateman = 500;
+    const expenses = gateman + stuff * lunchMoney;
+    const earning = ticketSale * ticketPrice;
+    const saving = earning - expenses;
+    return saving;
+  }
 }
-const result = calculateMoney(93);
+const result = calculateMoney(-130);
 console.log(result);
