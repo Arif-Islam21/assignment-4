94559172e0c576fcecf008d159e1a49ee03b033d
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-07 21:32:48 +0600

    basic task 1 done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+function calculateMoney(ticketSale) {
+  const ticketPrice = 120;
+  const lunchMoney = 50;
+  const stuff = 8;
+  const gateman = 500;
+  const expenses = gateman + stuff * lunchMoney;
+  const earning = ticketSale * ticketPrice;
+  const saving = earning - expenses;
+  return saving;
+}
+const result = calculateMoney(93);
+console.log(result);
